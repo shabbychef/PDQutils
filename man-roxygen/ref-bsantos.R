5ccff0b02579bdc6df29b4a7542fd9105fc32396
Steven Pav <shabbychef@gmail.com>
2015-06-08 20:03:57 -0700

    pickup work on package

---
+++ man-roxygen/ref-bsantos.R
@@ -0,0 +1,7 @@
+#' @references
+#'
+#' M. N. Berberan-Santos. "Expressing a Probability Density Function in Terms of another 
+#' PDF: A Generalized Gram-Charlier Expansion." Journal of Mathematical
+#' Chemistry 42, no 3 (2007): 585-594.
+#' \url{http://web.ist.utl.pt/ist12219/data/115.pdf}
+#'
